1d3e17881a574849d2a0377bc8450c39bd1efd6c
tirantvcia <tirantvcia@gmail.com>
2023-04-12 22:14:44 +0200

    Cadena original vacia, devuelve vacio, rojo

---
+++ src/tests/kata/wordWrap/wordWrap.test.ts
@@ -0,0 +1,21 @@
+/*
+    wordWrap('',5) ⇒ ''
+    wordWrap('hello',5) ⇒ 'hello'
+    wordWrap('longword',4) ⇒ 'long\nword'
+    wordWrap('reallylongword',4) ⇒ 'real\nlylo\nngwo\nrd'
+    wordWrap('abc def',4) ⇒ 'abc\ndef' 
+    wordWrap('abc def ghi',4) ⇒ 'abc\ndef\nghi'
+    wordWrap(' abcdf',4) ⇒ '\nabcd\nf'
+    wordWrap(null,5) ⇒ ''
+    wordWrap('hello',-5) ⇒ throw exception
+*/
+describe("WordWrap functionallity", () => {
+    it("Cadena original vacia, devuelve vacio", () => {
+        const result = wordWrap('', 5);
+        expect(result).toBe('');
+    })
+});
+
+function wordWrap(cadenaOriginal: string, numeroCaracteresLinea: number) {
+    throw new Error("Function not implemented.");
+}
