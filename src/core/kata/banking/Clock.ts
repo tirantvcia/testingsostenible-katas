a6b071bf60e78e51b38ed9298ad528fcbf41a81f
tirantvcia <tirantvcia@gmail.com>
2023-05-01 00:55:26 +0200

    verde. The Clock, devuelve el dia en formato correcto dd/MM/yyyy

--- src/core/kata/banking/Clock.ts
+++ src/core/kata/banking/Clock.ts
@@ -1,5 +1,13 @@
 export class Clock {
   todayAsString() {
-    return "";
+    const today = this.today();
+    return today.toLocaleString("es-ES", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+  }
+  protected today() {
+    return new Date();
   }
 }
--- src/tests/kata/banking/unit/Clock.test.ts
+++ src/tests/kata/banking/unit/Clock.test.ts
@@ -1,7 +1,14 @@
 import { Clock } from "../../../../core/kata/banking/Clock";
+
+class TestableClock extends Clock {
+  protected today(): Date {
+    return new Date("2022-02-25");
+  }
+}
+
 describe("The Clock", () => {
   it("devuelve el dia en formato correcto dd/MM/yyyy", () => {
-    const clock = new Clock();
+    const clock = new TestableClock();
     const date = clock.todayAsString();
     expect(date).toEqual("25/02/2022");
   });
