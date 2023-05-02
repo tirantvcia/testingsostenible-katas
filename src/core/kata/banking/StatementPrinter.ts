46572b9f36e78bbd8580843e32c6516698d2de14
tirantvcia <tirantvcia@gmail.com>
2023-05-02 17:41:20 +0200

    verde,unit test printStatement siempre imprime la cabecera a través de la consola. Refactor

--- src/core/kata/banking/StatementPrinter.ts
+++ src/core/kata/banking/StatementPrinter.ts
@@ -2,7 +2,8 @@ import { Transaction } from "./Transaction";
 import { Console } from "./console";
 export class StatementPrinter {
   constructor(private console: Console) {}
+  private readonly header = "Date       | Amount    | Balance";
   print(transacionts: Transaction[]) {
-    this.console.log("Date       | Amount    | Balance");
+    this.console.log(this.header);
   }
 }
