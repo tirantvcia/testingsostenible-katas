d401d43e8ec14f77d1613479ee49c159f183b090
tirantvcia <tirantvcia@gmail.com>
2023-05-02 17:51:49 +0200

    rojo,unit test printStatement imprime una transacción a través de la consola

--- src/tests/kata/banking/unit/printStament.test.ts
+++ src/tests/kata/banking/unit/printStament.test.ts
@@ -1,6 +1,7 @@
 import { Console } from "../../../../core/kata/banking/console";
 import { StatementPrinter } from "../../../../core/kata/banking/StatementPrinter";
 import { Transaction } from "../../../../core/kata/banking/Transaction";
+import { Clock } from "../../../../core/kata/banking/Clock";
 describe("The printStatement", () => {
   const console = new Console();
   let consoleSpy = jest.spyOn(console, "log");
@@ -9,5 +10,14 @@ describe("The printStatement", () => {
     let transactions: Transaction[] = [];
     statementPrinter.print(transactions);
     expect(consoleSpy).toHaveBeenCalledWith("Date       | Amount    | Balance");
+  });
+  it("imprime una transacción a través de la consola", () => {
+    const today = "14/01/2022";
+    const amount = 2000.0;
+
+    const transactions = [new Transaction(today, amount)];
+    statementPrinter.print(transactions);
+    expect(consoleSpy).toHaveBeenCalledWith("Date       | Amount    | Balance");
+    expect(consoleSpy).toHaveBeenCalledWith("14/01/2022 | 2000.00   | 2000.00");
   });
 });
