d421665230484b9a95d0dac252d9fbbce7205bee
tirantvcia <tirantvcia@gmail.com>
2023-05-02 22:37:44 +0200

    StatementPrinter, rojo: imprime múltiples transacciones a través de la consola

--- src/tests/kata/banking/unit/printStament.test.ts
+++ src/tests/kata/banking/unit/printStament.test.ts
@@ -20,4 +20,18 @@ describe("The printStatement", () => {
     expect(consoleSpy).toHaveBeenCalledWith("Date       | Amount    | Balance");
     expect(consoleSpy).toHaveBeenCalledWith("14/01/2022 | 2000.00   | 2000.00");
   });
+  it("imprime múltiples transacciones a través de la consola", () => {
+    const transactions = [
+      new Transaction("14/01/2022", 2000.0),
+      new Transaction("15/01/2022", -1000.0),
+      new Transaction("16/01/2022", 500.0),
+    ];
+    statementPrinter.print(transactions);
+    expect(consoleSpy).toHaveBeenCalledWith("Date       | Amount    | Balance");
+    expect(consoleSpy).toHaveBeenCalledWith("16/01/2022 | 500.00   | 1500.00");
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "15/01/2022 | -1000.00   | 1000.00"
+    );
+    expect(consoleSpy).toHaveBeenCalledWith("14/01/2022 | 2000.00   | 2000.00");
+  });
 });
